d0e0bb61a257584a6f664548ab708d1e5d1facaf
stevensotelo <stevenbetancurt@hotmail.com>
2022-04-11 08:34:31 -0500

    fixed about page

--- src/agroadvisory/src/pages/fertilization/Fertilization.js
+++ src/agroadvisory/src/pages/fertilization/Fertilization.js
@@ -30,8 +30,20 @@ function Fertilization() {
             <Sidebar opt_forecast={opt_forecast} opt_crops={opt_crops} opt_scenarios={opt_scenarios} OnChangeForecast={changeForecast} OnChangeCrop={changeCrop} OnChangeScenario={changeScenario} />
             <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <MapHeader title={"Type yield"} />
+                <p className='text-justify'>
+                    The tool provides many types of yield for a given (optimal nutrient amount). This includes:
+                    <ol>
+                        <li>Agronomic optimal yield</li>
+                        <li>Agronomic attainable yield</li>
+                        <li>Current yield (blanket yield)</li>
+                        <li>Economic/profitable optimal yield based on which yield decomposition can also be calculated</li>
+                    </ol>
+                </p>
                 <Map id="map_type_yield" init={map_init} type={"yield"} crop={crop} forecast={forecast} scenario={scenario} />
                 <MapHeader title={"Nutrients"} />
+                <p className='text-justify'>
+                    Optimal nutrient amount (N, P, K) which accounts their interaction effect on yield.
+                </p>
                 <Map id="map_nutrients" init={map_init} type={"nutrients"} crop={crop} forecast={forecast} scenario={scenario} />
             </main>
         </div>
