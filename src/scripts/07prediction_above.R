ed35e01a8601be049ff785cce61acd119a00e154
Tilaye <amsalutilaye2019@gmail.co>
2022-04-12 14:46:50 +0300

    all scripts for normal, below, above added

---
+++ src/scripts/07prediction_above.R
@@ -0,0 +1,101 @@
+# ------------------------------------------------------------------------------
+# souring data preparation script
+
+  rm(list = ls())
+  setwd("/workdir/script")
+  source("input.R")
+
+# ------------------------------------------------------------------------------
+# extracting values by points
+  message(noquote("Extracting point vaues..."))
+  cov_soil_land <- stack_above[[1:16]]
+  grid_val <- extract(cov_soil_land, points)
+  cov_train <- cbind(grid_val, points_train) # covariates, yield & nps
+  cov_train <- cov_train[complete.cases(cov_train),]
+  cov_train$landform <- as.factor(cov_train$landform)
+  yield_nps <- dplyr::select(cov_train, c("n","p","k", "yield"))
+  cov_train <- dplyr::select(cov_train, -c("n","p","k", "yield"))
+  cov_train <- cbind(cov_train, yield_nps)
+  cov_train <- unique(na.omit(cov_train[, 1:ncol(cov_train)])) #removing NAs and duplicates
+  
+  # dir.create("workspace")
+  setwd("/workdir/data/workspace")
+  save(cov_train, file = paste0("regression_matrix_above", ".RData"))
+  
+  #load the rda file
+  # load(file = "./output/regression_matrix.rda")#loads the global env with the file
+  
+  # training
+  mtry <- as.integer((ncol(cov_train))/3) #this will be optimized
+  mtry <- seq(mtry-8, mtry+8, by = 2)
+  
+  rf_fitControl <- trainControl(method = "repeatedcv",
+                                number = 10,
+                                repeats = 5)
+  
+  rf_tuneGrid <- expand.grid(.mtry = mtry,
+                             .splitrule =  "maxstat",
+                             .min.node.size = c(20, 30))
+  
+  inTrain <- createDataPartition(y =  cov_train$yield, p = 0.70, list = FALSE)
+  training <- cov_train[inTrain,]
+  testing <- cov_train[-inTrain,]
+  
+  message(noquote("Training the model..."))
+  mod_fit <- train(
+    yield ~ .,
+    data = training,
+    method = "ranger",
+    trControl = rf_fitControl,
+    importance = 'impurity',
+    tuneGrid = rf_tuneGrid,
+    preProcess = c('scale', 'center'))
+  
+  # setwd(workspace)
+  save(mod_fit, file = paste0("model_above", ".RData"), Overwrite = T)
+  # load(file = "./output/model.RData")
+# ------------------------------------------------------------------------------  
+# Plot and save variable importance and testing
+  var_imp <- varImp(mod_fit)
+  ggplot(var_imp)
+  
+  ggsave(filename = "variable_importance_above.png", width = 20, height = 10)
+  
+# ------------------------------------------------------------------------------  
+# Multiple iteration
+
+  N <- c(seq(0, 75, 15), seq(100, 200, 25)) #this will be optimized
+  P <- K <- N[1:7]
+  # P <- K <- N[1:3]
+  npk <- expand.grid(N = N, P = P, K = K)
+  
+#loop for climate forcast scenario
+# setwd(workspace)
+  path <- "yield_above"
+  dir.create(path, FALSE, TRUE)
+  path = "/workdir/data/workspace/yield_above"
+  a <- apply(npk, 1, function(i) paste(i, collapse="."))
+  f <- file.path(path, paste0("yield.", a, ".tif"))
+  setwd("/workdir/data/workspace/yield_above")
+  # message(noquote("Predicting ..."))
+  message(noquote("Predicting yield"))
+  #progress bar
+  n_iter <- nrow(npk)
+  pb <- txtProgressBar(min = 0, max = n_iter, style = 3, width = 50, char = "=")
+  for (i in 1:nrow(npk)) {
+    # progress(i, progress.bar=TRUE)
+    if (file.exists(f[i])) next
+    NPK <- data.frame(n = npk$N[i], p = npk$P[i], k = npk$K[i])
+      predict(
+        stack_above, 
+        mod_fit,
+        const = NPK,
+        filename = f[i],
+        overwrite = TRUE,
+        wopt = list(datatype = "INT2S", names = a[i])
+      )
+      # if (i == nrow(npk)) message("Done!")
+      setTxtProgressBar(pb, i)
+  }
+  close(pb)
+  
---
+++ src/scripts/09prediction_attainable_yield_above.R
@@ -0,0 +1,62 @@
+# ------------------------------------------------------------------------------
+# souring data preparation script
+
+  rm(list = ls())
+
+  setwd("/workdir/script")
+  source("input.R")
+  
+  setwd("/workdir/data/final")
+  n <- raster("wheat_n_above.tif")
+  p <- raster("wheat_p_above.tif")
+  k <- raster("wheat_k_above.tif")
+  # s <- raster("wheat_S_potential.tif")
+  yield_pot <- raster("wheat_optimal_yield_above.tif")
+  
+# ------------------------------------------------------------------------------ 
+# stack rasters
+  cov <- stack(stack_above, n,p, k, yield_pot)
+  names(cov)[29:32] <- c("n", "p","k", "yield")
+
+# ------------------------------------------------------------------------------
+# extracting values by points
+  message(noquote("Extracting raster values ..."))
+  grid_val <- extract(cov, points)
+  grid_val <- as.data.frame(grid_val)
+  grid_val <-  unique(na.omit(grid_val[, 1:ncol(grid_val)]))
+  
+  inTrain <- sample(nrow(grid_val), 0.8 * nrow(grid_val))
+  Xtraining     <- grid_val[inTrain,c(1:31)]
+  Xtesting      <- grid_val[-inTrain,c(1:31)]
+  Ytraining     <- grid_val[inTrain,32]
+  Ytesting      <- grid_val[-inTrain,32]
+  
+  message(noquote("Training the model ..."))
+  mod_fit <- quantregForest(x = Xtraining, y = Ytraining)
+  
+  message(noquote("Predicting attainable yield ..."))
+  pred_90 <- predict(object = cov, model = mod_fit, what =  0.95)
+  
+  message(noquote("Writing rasters and csv files ..."))
+  setwd("/workdir/data/final")
+  
+  writeRaster(
+    pred_90,
+    filename = "wheat_attainable_yield_above",
+    format = "GTiff",
+    overwrite = TRUE)
+  
+  pred_sgdf <- as(pred_90, "SpatialGridDataFrame") 
+  pred_sgdf <- as.data.frame(pred_sgdf)
+  n <- as(n, "SpatialGridDataFrame")
+  n <- as.data.frame(n)
+  p <- as(p, "SpatialGridDataFrame")
+  p <- as.data.frame(p)
+  k <- as(k, "SpatialGridDataFrame")
+  k <- as.data.frame(k)
+  
+  csv_attainable <- cbind(pred_sgdf[,1], n[,1], p[,1], k)
+  
+  colnames(csv_attainable) <- c("attainable_yield","n","p","k","lon","lat")
+  write.csv(csv_attainable, file = "wheat_attainable_yield_above.csv", row.names = F, sep = ",")
+  
