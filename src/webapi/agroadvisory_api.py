f479e04d1fe32be09b8cbd7bb3b0e80a02b5b633
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2022-05-17 16:30:06 -0500

    Working on getting data with WFS

--- src/webapi/agroadvisory_api.py
+++ src/webapi/agroadvisory_api.py
@@ -1,22 +1,39 @@
 
 import os
 import sys
+
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin, CORS
 from geoserver.catalog import Catalog
+from owslib.wfs import WebFeatureService
+import geopandas as gpd
+
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-geo_url = "https://geo.aclimate.org/geoserver/rest/"
+workspace = "test_kebele/"
+service = "wfs"
+geo_url = "http://localhost:8600/geoserver/"+workspace + \
+    service  # "https://geo.aclimate.org/geoserver/rest/"
+print(geo_url)
 geo_user = 'admin'
 geo_pwd = 'geoserver'
 
-cat = Catalog("http://localhost:8600/geoserver/rest",
-              username=geo_user, password=geo_pwd)
-that_layer = cat.get_resource("sample_kebele")
-print(that_layer.name)
+try:
+
+    # cat = Catalog("http://localhost:8600/geoserver/rest/",
+    #               username=geo_user, password=geo_pwd)
+    # print("Connected")
+    # that_layer = cat.get_resource(
+    #     "sample_kebele", workspace="test_kebele")
+    # print(that_layer)
+    wfs = WebFeatureService(url=geo_url)
+
+except Exception as err:
+    error = str(err).split()[50:61]
+    print(" ".join(error))
 
 
 @app.route('/test', methods=['GET', 'POST'])
