816e170bf3849338a37f2110fe0dfc73da937d4f
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2022-05-25 11:39:36 -0500

    Delete WoredaList

--- src/webapi/agroadvisory_api.py
+++ src/webapi/agroadvisory_api.py
@@ -6,14 +6,15 @@ from flask_restful import Resource, Api, fields, marshal_with
 from flasgger import Swagger
 
 from api_modules.kebele.kebele import Kebele
-from api_modules.woreda.woreda import Woreda, WoredaList
+from api_modules.woreda.woreda import Woreda
 
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
 
-api.add_resource(WoredaList, '/woredas')
-api.add_resource(Woreda, '/woredas/<woreda_id>')
+#api.add_resource(WoredaList, '/woredas')
+api.add_resource(Woreda, '/woredas', endpoint="woredas")
+api.add_resource(Woreda, '/woredas/<woreda_id>', endpoint="woreda")
 api.add_resource(Kebele, '/kebele/<kebele_id>')
 
 if __name__ == '__main__':
