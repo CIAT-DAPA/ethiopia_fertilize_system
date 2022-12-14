be7d2f1abe1becb104b7243e445829e1aef9a5be
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2022-12-14 09:57:49 -0500

    map changed to satelital

--- src/agroadvisory/src/components/map/Map.js
+++ src/agroadvisory/src/components/map/Map.js
@@ -178,7 +178,7 @@ function Map(props) {
             <MapContainer center={props.init.center} zoom={props.init.zoom} zoomControl={false} style={props.style} scrollWheelZoom={true} whenReady={handleEventsMap}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
                     
                 />
                 <LayersControl position="topright" collapsed={false}>
