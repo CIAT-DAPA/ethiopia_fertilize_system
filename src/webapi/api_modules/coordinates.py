0a13339b59acc1000ac79706377a94300a728922
carlosn97 <carlosnasayov@gmail.com>
2022-12-20 16:57:10 -0500

    endpoint recibe array and response with array on objects working

--- src/webapi/api_modules/coordinates.py
+++ src/webapi/api_modules/coordinates.py
@@ -2,17 +2,17 @@ import os
 from flask_restful import Resource
 from URLSearchParams import URLSearchParams
 import requests
-
-
+import json
+import ast
 GEOSERVER_URL="https://geo.aclimate.org/geoserver/fertilizer_et/";
 SERVICEE="wms";
+lista=[]
 class Coordinates(Resource):
 
 
     def __init__(self):
         super().__init__()
-
-    def get(self,layer=None,coor=None):
+    def post(self,layer=None,coor=None):
         """
         Get Features
 
@@ -24,9 +24,10 @@ class Coordinates(Resource):
             required: false
           - in: path
             name: coor
-            type: string
+            type: array
+            items:
+                type: object
             required: false
-         
         responses:
           200:
             description: Latitude, longitude and value
@@ -43,33 +44,44 @@ class Coordinates(Resource):
                   default: 0.00
                 
         """
-        cor=coor.split(',')
-        lon=float(cor[0]);
-        lat=float(cor[1]);
-        parameters={
-            'service':'WMS',
-            'version':'1.1.1',
-            'request':'GetFeatureInfo',
-            'layers':layer,
-            'query_layers':layer,
-            'feature_count':10,
-            'info_format':'application/json',
-            'format_options':'callback:handleJson',
-            'SrsName':'EPSG:4326',
-            'width':101,
-            'height':101,
-            'x':50,
-            'y':50,
-            'bbox':str(lon - 0.1) + ',' + str(lat - 0.1) + ',' + str(lon + 0.1) + ',' + str(lat + 0.1)}
+        arr=ast.literal_eval(coor)
+        print(arr[0]['lat'])
+        print(len(arr))
+        pr=[]
+        for i in arr:
+            lat=i['lat']
+            lon=i['lon']
+            pr.append(lon)
+            parameters={
+                'service':'WMS',
+                'version':'1.1.1',
+                'request':'GetFeatureInfo',
+                'layers':layer,
+                'query_layers':layer,
+                'feature_count':10,
+                'info_format':'application/json',
+                'format_options':'callback:handleJson',
+                'SrsName':'EPSG:4326',
+                'width':101,
+                'height':101,
+                'x':50,
+                'y':50,
+                'bbox':str(lon - 0.1) + ',' + str(lat - 0.1) + ',' + str(lon + 0.1) + ',' + str(lat + 0.1)}
 
-        self.url=str(URLSearchParams(GEOSERVER_URL + SERVICEE).append(parameters))
-        print(self.url)
-        response= requests.get(self.url)
-        
-        data= response.json()
-        json= data
-        respuesta=[{'lat':lat,'lon':lon,'value':json['features'][0]['properties']['GRAY_INDEX']}]
-        return respuesta
+            self.url=str(URLSearchParams(GEOSERVER_URL + SERVICEE).append(parameters))
+            print(self.url)
+            
+            response= requests.get(self.url)
+            data= response.json()
+            json= data
+            
+            respuesta={'lat':lat,'lon':lon,'value':json['features'][0]['properties']['GRAY_INDEX']}
+            if not lista.__contains__(respuesta):
+                lista.append(respuesta)
+            
+        return lista
+
+            
         
 
        
