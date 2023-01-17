55a348aba6c23c0974fb23549a8c2d6435a1f1e9
santiagoC <s.calderon@cgiar.org>
2023-01-17 14:11:59 -0500

    test vs

--- src/agroadvisory/src/App.test.js
+++ src/agroadvisory/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/NextGen Agroadvisory/i);
+  const linkElement = screen.getByText(/Fertilizer NPS & Urea/i);
   expect(linkElement).toBeInTheDocument();
 });
