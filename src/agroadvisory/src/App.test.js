4ac690893fb373a449c7fa37fa5fcce80e4420f5
carlosn97 <carlosnasayov@gmail.com>
2023-01-24 10:34:29 -0500

    urea and nps added

--- src/agroadvisory/src/App.test.js
+++ src/agroadvisory/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Fertilizer NPS & Urea/i);
+  const linkElement = screen.getByText(/NPS & Urea/i);
   expect(linkElement).toBeInTheDocument();
 });
